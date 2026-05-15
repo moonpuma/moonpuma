2947b03baa4c23b78d6e59ce50130cd13f8c58df
mikita_bialko <bloodyrainday@gmail.com>
2026-05-15 11:41:19 +0300

    feat(ui): add varient prop and apply styles in className prop

--- src/shared/ui/Button/Button.tsx
+++ src/shared/ui/Button/Button.tsx
@@ -1,13 +1,26 @@
+import s from "./Button.module.css";
+
 type PropsType = {
   className?: string;
   disabled?: boolean;
   title: string;
+  variant?: "filled" | "outlined" | "secondary";
   onClick: () => void;
 };
 
-export const Button = ({ className, disabled, title, onClick }: PropsType) => {
+export const Button = ({
+  className,
+  disabled,
+  title,
+  variant = "filled",
+  onClick,
+}: PropsType) => {
   return (
-    <button className={className} onClick={onClick}>
+    <button
+      className={`${s[variant]} ${className || ""}`}
+      onClick={onClick}
+      disabled={disabled}
+    >
       {title}
     </button>
   );
