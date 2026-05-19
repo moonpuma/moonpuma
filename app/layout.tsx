4799bdc845f482827fb3a7e28223fd670c38cc1d
Bohdan Honcharenko <bogdan.goncharenko.03@gmail.com>
2026-05-19 12:13:49 +0200

    add font

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,13 @@
 import type { Metadata } from "next";
+import { Inter } from "next/font/google";
 import "@/app/styles/globals.scss";
+
+const inter = Inter({
+  subsets: ["latin", "cyrillic"],
+  weight: ["300", "400", "500", "600", "700"],
+  variable: "--font-inter",
+  display: "swap",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -12,7 +20,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="en" className={inter.variable}>
       <body>{children}</body>
     </html>
   );
