9aa2fdf69c2c912e82900ecbe3fd7e611dc6845d
Bohdan Honcharenko <bogdan.goncharenko.03@gmail.com>
2026-05-31 23:29:37 +0200

    change next config and pnpm worksapce

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,10 @@
+import path from 'path'
 import type { NextConfig } from 'next'
 
 const nextConfig: NextConfig = {
   reactCompiler: true,
   turbopack: {
-    root: '.',
+    root: path.resolve(),
   },
 }
 
