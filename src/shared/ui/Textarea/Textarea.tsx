9498c9b72dc96ea1e530257bff8b91ab293ade83
mikita_bialko <bloodyrainday@gmail.com>
2026-06-02 14:23:20 +0300

    feat(ui): apply styles to Textarea component

--- src/shared/ui/Textarea/Textarea.tsx
+++ src/shared/ui/Textarea/Textarea.tsx
@@ -1,11 +1,12 @@
 'use client'
 
 import { TextareaHTMLAttributes } from 'react'
+import s from './Textarea.module.css'
 
 type PropsType = {
   className?: string
 } & TextareaHTMLAttributes<HTMLTextAreaElement>
 
 export const Textarea = ({ className, ...restProps }: PropsType) => {
-  return <textarea className={className} {...restProps}></textarea>
+  return <textarea className={`${s.textarea} ${className || ''}`} {...restProps}></textarea>
 }
