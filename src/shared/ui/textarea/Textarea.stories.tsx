c38bf9587901c347f1602f34b9a43a1d1739fe2d
an-kulinich <an.kulinich96@gmail.com>
2026-06-15 21:06:08 +0300

    Change project structure

--- src/shared/ui/textarea/Textarea.stories.tsx
+++ src/shared/ui/textarea/Textarea.stories.tsx
@@ -2,7 +2,7 @@ import type { Meta, StoryObj } from '@storybook/nextjs-vite'
 import { Textarea } from './Textarea'
 
 const meta: Meta<typeof Textarea> = {
-    title: 'Shared/Textarea',
+    title: 'Components/Textarea',
     component: Textarea,
     tags: ['autodocs'],
     argTypes: {
